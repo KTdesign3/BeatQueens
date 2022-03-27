011724240adf9263f387d8f9358160ed6a366d6d
KTdesign3 <sodenkt@gmail.com>
2022-03-27 15:54:46 +0100

    Pellet destruction switch
    Switch pellet destroyer asset.

--- Assets/BeatQueens_Assembly/Scripts/Core/PelletScript.cs
+++ Assets/BeatQueens_Assembly/Scripts/Core/PelletScript.cs
@@ -9,8 +9,9 @@ public class PelletScript : MonoBehaviour
     public float PelletBigTimeLeft = 3;
     public bool PelletBigTimerActive;
     public bool PelletBigTimerMade;
-    public GameObject Pellet;
-   
+    //public GameObject Pellet;
+    GameObject ProjectilePrefab;
+
 
 
     // Start is called before the first frame update
@@ -22,7 +23,7 @@ public class PelletScript : MonoBehaviour
     public void BigPelletStart()
     {
         PelletBigTimerMade = true;
-
+        ProjectilePrefab = Instantiate(ProjectilePrefab);
 
     }
 
@@ -44,8 +45,12 @@ public class PelletScript : MonoBehaviour
         PelletBigTimeLeft -= Time.deltaTime;
         if (PelletBigTimeLeft <= 0) //This will explode the object after ExpTimeLeft hits 0.
         {
-            Destroy(Pellet);
+
+            //Destroy(Pellet);
+
             //  Destroy(Bomb);
+            //Destroy(PelletInstantiated);
+            Destroy(ProjectilePrefab, 2.0f);
             Debug.Log("DESTROY PELLET");
             PelletBigTimerMade = false;
             PelletBigTimeLeft = 3;
